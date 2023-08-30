55fd7452d813f2c5a08fbee6a9aefb8aaf57df03
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-30 19:36:07 +0000

    add bazel dockerfile

---
+++ bazel/generate.ts
@@ -0,0 +1,14 @@
+import { Dockerfile } from "https://deno.land/x/fluentdocker/mod.ts";
+
+const image = new Dockerfile()
+  .from("openjdk:22-slim-bookworm")
+  .run("apt-get update")
+  .run("apt-get install -y wget build-essential")
+  .run(
+    "wget -O /usr/local/bin/bazelisk https://github.com/bazelbuild/bazelisk/releases/download/v1.18.0/bazelisk-linux-amd64"
+  )
+  .cmd("bazelisk --version");
+
+const dockerfile = image.toString();
+
+console.log(dockerfile);
