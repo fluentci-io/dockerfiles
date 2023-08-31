95f28ef156e5c4dca38ad5d91987b7313fe6dc03
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-31 09:18:23 +0000

    reuse nix image from fluentci

--- devbox/generate.ts
+++ devbox/generate.ts
@@ -1,16 +1,7 @@
-import { Dockerfile } from "https://deno.land/x/fluentdocker/mod.ts";
+import { Dockerfile } from "https://deno.land/x/fluentdocker@v0.1.1/mod.ts";
 
 const image = new Dockerfile()
-  .from("alpine:latest")
-  .run("apk update")
-  .run("apk add curl bash python3 alpine-sdk")
-  .run(
-    'curl --proto =https --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --extra-conf "sandbox = false" --init none --no-confirm'
-  )
-  .env("PATH", "${PATH}:/nix/var/nix/profiles/default/bin")
-  .run(
-    "sed -i 's/auto-allocate-uids = true/auto-allocate-uids = false/g' /etc/nix/nix.conf"
-  )
+  .from("ghcr.io/fluent-ci-templates/nix:latest")
   .run("adduser --disabled-password devbox")
   .run("addgroup devbox nixbld")
   .env("FORCE", "1")
