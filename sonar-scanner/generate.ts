e247391d85ba69100f69028729dda47395574797
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-09-16 16:27:15 +0000

    [sonar-scanner] add nodejs to the image

--- sonar-scanner/generate.ts
+++ sonar-scanner/generate.ts
@@ -2,7 +2,7 @@ import { Dockerfile } from "https://deno.land/x/fluentdocker@v0.1.1/mod.ts";
 
 const image = new Dockerfile()
   .from("ghcr.io/fluent-ci-templates/devbox:latest")
-  .run("devbox global add sonar-scanner-cli")
+  .run("devbox global add sonar-scanner-cli nodejs")
   .run('eval "$(devbox global shellenv)" && sonar-scanner --version')
   .cmd('eval "$(devbox global shellenv)" && sonar-scanner');
 
