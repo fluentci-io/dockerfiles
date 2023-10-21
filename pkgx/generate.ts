617c57fbbf9ef76f9a263d383e5e0e16c17f1444
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-21 14:47:14 +0000

    update pkgx image

--- pkgx/generate.ts
+++ pkgx/generate.ts
@@ -1,10 +1,15 @@
 import { Dockerfile } from "https://deno.land/x/fluentdocker@v0.1.1/mod.ts";
 
 const image = new Dockerfile()
-  .from("homebrew/brew:latest")
-  .run("brew install pkgxdev/made/pkgx")
+  .from("ubuntu:23.04")
+  .run("apt-get update")
+  .run("apt-get install -y curl")
+  .run("curl https://pkgx.sh | sh")
+  .copy(".bashrc", "/root/.bashrc")
+  .env("BASH_ENV", "/root/.bashrc")
+  .shell(["bash", "-c"])
   .run("pkgx --version")
-  .cmd("pkgx");
+  .cmd("bash -i");
 
 const dockerfile = image.toString();
 
