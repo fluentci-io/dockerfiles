00dc090076e98ac9b4d94a9d62dfb6b7d7cf6eb4
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-04 05:33:06 +0000

    fix gleam & node images

--- gleam/generate.ts
+++ gleam/generate.ts
@@ -1,23 +1,11 @@
 import { Dockerfile } from "https://deno.land/x/fluentdocker@v0.1.1/mod.ts";
 
 const image = new Dockerfile()
-  .from("alpine:latest")
-  .run("apk update")
-  .run("apk add curl bash python3 alpine-sdk")
-  .run(
-    'curl --proto =https --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --extra-conf "sandbox = false" --init none --no-confirm'
-  )
-  .env("PATH", "${PATH}:/nix/var/nix/profiles/default/bin")
-  .run(
-    "sed -i 's/auto-allocate-uids = true/auto-allocate-uids = false/g' /etc/nix/nix.conf"
-  )
-  .run("adduser --disabled-password devbox")
-  .run("addgroup devbox nixbld")
-  .env("FORCE", "1")
-  .run("curl -fsSL https://get.jetpack.io/devbox | bash")
-  .run("devbox global add gleam erlang rebar3")
-  .run('eval "$(devbox global shellenv)" && gleam --version')
-  .cmd("devbox version");
+  .from("pkgxdev/pkgx:latest")
+  .run("apt-get update")
+  .run("apt-get install -y ca-certificates")
+  .run("pkgx install gleam escript")
+  .run("gleam --version");
 
 const dockerfile = image.toString();
 
--- node/generate.ts
+++ node/generate.ts
@@ -1,23 +1,14 @@
 import { Dockerfile } from "https://deno.land/x/fluentdocker@v0.1.1/mod.ts";
 
 const image = new Dockerfile()
-  .from("alpine:latest")
-  .run("apk update")
-  .run("apk add curl bash python3 alpine-sdk")
-  .run(
-    'curl --proto =https --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --extra-conf "sandbox = false" --init none --no-confirm'
-  )
-  .env("PATH", "${PATH}:/nix/var/nix/profiles/default/bin")
-  .run(
-    "sed -i 's/auto-allocate-uids = true/auto-allocate-uids = false/g' /etc/nix/nix.conf"
-  )
-  .run("adduser --disabled-password devbox")
-  .run("addgroup devbox nixbld")
-  .env("FORCE", "1")
-  .run("curl -fsSL https://get.jetpack.io/devbox | bash")
-  .run("devbox global add yarn nodejs@18.16.1")
-  .run('eval "$(devbox global shellenv)" && node --version')
-  .cmd("devbox version");
+  .from("pkgxdev/pkgx:latest")
+  .run("pkgx install node npm bun pnpm classic.yarnpkg.com rtx")
+  .run("node --version")
+  .run("npm --version")
+  .run("bun --version")
+  .run("pnpm --version")
+  .run("yarn --version")
+  .run("rtx --version");
 
 const dockerfile = image.toString();
 
