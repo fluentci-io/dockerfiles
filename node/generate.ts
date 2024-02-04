67dba6c7da7d36c19a14559a61aa3dc85101529d
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-04 05:36:07 +0000

    fix node image

--- node/generate.ts
+++ node/generate.ts
@@ -2,13 +2,15 @@ import { Dockerfile } from "https://deno.land/x/fluentdocker@v0.1.1/mod.ts";
 
 const image = new Dockerfile()
   .from("pkgxdev/pkgx:latest")
-  .run("pkgx install node npm bun pnpm classic.yarnpkg.com rtx")
+  .run("pkgx install node npm bun pnpm classic.yarnpkg.com rtx python git")
   .run("node --version")
   .run("npm --version")
   .run("bun --version")
   .run("pnpm --version")
   .run("yarn --version")
-  .run("rtx --version");
+  .run("rtx --version")
+  .run("python --version")
+  .run("git --version");
 
 const dockerfile = image.toString();
 
