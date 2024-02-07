2e829ac844c312efab67d798925ba7a392438b3b
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-07 08:51:53 +0300

    Update generate.ts

--- bun/generate.ts
+++ bun/generate.ts
@@ -3,7 +3,7 @@ import { Dockerfile } from "https://deno.land/x/fluentdocker@v0.1.1/mod.ts";
 const image = new Dockerfile()
   .from("ghcr.io/fluentci-io/devbox:latest")
   .run("devbox version")
-  .run("devbox global add yarn nodejs@18.17.1 bun@1.0.0")
+  .run("devbox global add yarn nodejs@18.17.1 bun")
   .run('eval "$(devbox global shellenv --recompute)" && bun --version')
   .run('eval "$(devbox global shellenv --recompute)" && node --version')
   .cmd('eval "$(devbox global shellenv --recompute)" && bun');
