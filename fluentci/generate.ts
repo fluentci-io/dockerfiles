195f163cdd9e9103085ba4d4c9f4d31ee7758a3b
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-05-26 06:37:28 +0000

    update fluentci image

--- fluentci/generate.ts
+++ fluentci/generate.ts
@@ -3,9 +3,23 @@ import { Dockerfile } from "https://deno.land/x/fluentdocker@v0.1.1/mod.ts";
 const image = new Dockerfile()
   .from("denoland/deno:alpine")
   .run("apk update")
-  .run("apk add --no-cache curl docker-cli")
+  .run("apk add --no-cache curl docker-cli wget")
   .run(
-    "curl -L https://dl.dagger.io/dagger/install.sh | DAGGER_VERSION=0.9.3 sh"
+    "wget https://github.com/fluentci-io/fluentci-engine/releases/download/v0.4.1/fluentci-engine_v0.4.1_x86_64-unknown-linux-gnu.tar.gz"
+  )
+  .run("tar xvf fluentci-engine_v0.4.1_x86_64-unknown-linux-gnu.tar.gz")
+  .run("mv fluentci-engine /usr/local/bin")
+  .run("rm fluentci-engine_v0.4.1_x86_64-unknown-linux-gnu.tar.gz")
+  .run(
+    "wget https://dl.fluentci.io/fluentci-studio/v0.1.0/fluentci-studio_v0.1.0_x86_64-unknown-linux-gnu.tar.gz"
+  )
+  .run("tar xvf fluentci-studio_v0.1.0_x86_64-unknown-linux-gnu.tar.gz")
+  .run("mv fluentci-studio /usr/local/bin")
+  .run("rm fluentci-studio_v0.1.0_x86_64-unknown-linux-gnu.tar.gz")
+  .run("which fluentci-studio")
+  .run("fluentci-engine --version")
+  .run(
+    "curl -L https://dl.dagger.io/dagger/install.sh | DAGGER_VERSION=0.11.0 sh"
   )
   .run("mv bin/dagger /usr/local/bin")
   .run("dagger version")
