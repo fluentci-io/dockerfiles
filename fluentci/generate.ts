07e5c09db965edad0ec6ee914cf8cd179f3171ac
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-07-09 20:02:36 +0000

    update fluentci dockerfile

--- fluentci/generate.ts
+++ fluentci/generate.ts
@@ -17,17 +17,17 @@ const image = new Dockerfile()
   )
   .run("mkdir -p /home/${USER} && chown -R ${USER}:${USER} /home/${USER}")
   .run(
-    "wget https://github.com/fluentci-io/fluentci-engine/releases/download/v0.4.2/fluentci-engine_v0.4.2_x86_64-unknown-linux-gnu.tar.gz"
+    "wget https://github.com/fluentci-io/fluentci-engine/releases/download/v0.4.5/fluentci-engine_v0.4.5_x86_64-unknown-linux-gnu.tar.gz"
   )
-  .run("tar xvf fluentci-engine_v0.4.2_x86_64-unknown-linux-gnu.tar.gz")
+  .run("tar xvf fluentci-engine_v0.4.5_x86_64-unknown-linux-gnu.tar.gz")
   .run("mv fluentci-engine /usr/local/bin")
-  .run("rm fluentci-engine_v0.4.2_x86_64-unknown-linux-gnu.tar.gz")
+  .run("rm fluentci-engine_v0.4.5_x86_64-unknown-linux-gnu.tar.gz")
   .run(
-    "wget https://dl.fluentci.io/fluentci-studio/v0.1.3/fluentci-studio_v0.1.3_x86_64-unknown-linux-gnu.tar.gz"
+    "wget https://dl.fluentci.io/fluentci-studio/v0.1.6/fluentci-studio_v0.1.6_x86_64-unknown-linux-gnu.tar.gz"
   )
-  .run("tar xvf fluentci-studio_v0.1.3_x86_64-unknown-linux-gnu.tar.gz")
+  .run("tar xvf fluentci-studio_v0.1.6_x86_64-unknown-linux-gnu.tar.gz")
   .run("mv fluentci-studio /usr/local/bin")
-  .run("rm fluentci-studio_v0.1.3_x86_64-unknown-linux-gnu.tar.gz")
+  .run("rm fluentci-studio_v0.1.6_x86_64-unknown-linux-gnu.tar.gz")
   .run("which fluentci-studio")
   .run("fluentci-engine --version")
   .run(
