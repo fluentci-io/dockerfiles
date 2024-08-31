895a052c288ed45bb0e00fd2c4c1c59110a55e59
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-08-31 08:09:46 +0000

    fix(fluentci): fix docker permissions

--- fluentci/entry.sh
+++ fluentci/entry.sh
@@ -9,6 +9,7 @@ fi
 case "$1" in
     run | init | search | upgrade | cache | ls | list | agent | doctor | docs | man | agent | whoami | publish | env | login | project | studio )
     sudo dockerd-entrypoint.sh > /dev/null 2> /dev/null &
+    sleep 8
     sudo chown -R `whoami` /var/run/docker.sock
     sudo chown -R `whoami` ~/*
     sudo chown -R `whoami` ~/.fluentci ~/.cache ~/.cargo ~/.rustup ~/.local || true
