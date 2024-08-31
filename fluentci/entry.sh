5c8b0ebfe7bd0c5b89ede3451321c4b5df955331
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-08-31 07:52:03 +0000

    fix(fluentci): fix permissions

--- fluentci/entry.sh
+++ fluentci/entry.sh
@@ -10,6 +10,8 @@ case "$1" in
     run | init | search | upgrade | cache | ls | list | agent | doctor | docs | man | agent | whoami | publish | env | login | project | studio )
     dockerd-entrypoint.sh > /dev/null 2> /dev/null &
     sudo chown -R `whoami` /var/run/docker.sock
+    sudo chown -R `whoami` ~/*
+    sudo chown -R `whoami` ~/.fluentci ~/.cache ~/.cargo ~/.rustup ~/.local || true
     # if the first argument is a known fluentci command
     exec fluentci "$@";;
 esac
